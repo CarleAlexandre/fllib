de20ca90e8fb5dddf102057fc97f2adf63ac8a9d
fatmeat <pathofyggdrasil@gmail.com>
2023-12-07 12:20:41 +0100

    optimized vec 3 linear function with simd, need to test simd as my union of m128i and vec3 could be missaligned in my haed

--- include/fllinear.hh
+++ include/fllinear.hh
@@ -8,18 +8,118 @@
 
 namespace fl {
 
+//vec2
+
+inline float
+distance2(vec2 v1, vec2 v2) {
+    return (pow(v2.x - v1.x, 2) + pow(v2.y - v1.y, 2));
+}
+
+//vec3
+
 inline vec3
-vec3divValue(const fl::vec3 &v, float f) {
-    if (f != 0.0f) {
-        return ((vec3){v.x / f, v.y / f, v.z / f});
-    } else {
-		return ((vec3){0.0f, 0.0f, 0.0f});
-    }
+vec3add(const vec3 v1, const vec3 v2) {
+	return(vec3_mm_add((vec3_mm_t){.vec3 = v1}, (vec3_mm_t){.vec3 = v2}));
 }
 
 inline vec3
-vec3mulValue(const fl::vec3 &v, float f) {
-    return ((vec3){v.x * f, v.y * f, v.z * f});
+vec3sub(const vec3 v1, const vec3 v2) {
+	return(vec3_mm_sub((vec3_mm_t){.vec3 = v1}, (vec3_mm_t){.vec3 = v2}));
+}
+
+inline vec3
+vec3mul(const vec3 v1, const vec3 v2) {
+	return(vec3_mm_mul((vec3_mm_t){.vec3 = v1}, (vec3_mm_t){.vec3 = v2}));
+}
+
+inline vec3
+vec3addValue(const vec3 v, const float add) {
+	return(vec3_mm_add((vec3_mm_t){.vec3 = v}, (vec3_mm_t){.vec3 = {add, add, add}}));
+}
+
+inline vec3
+vec3subValue(const vec3 v, const float sub) {
+    return(vec3sub(v, {sub, sub, sub}));
+}
+
+inline vec3
+vec3mulValue(const vec3 v, float f) {
+    return (vec3mul(v, (vec3){f, f, f}));
+}
+
+inline vec3
+vec3scale(const vec3 v, const float scalar) {
+	return(vec3_mm_mul((vec3_mm_t){.vec3 = v}, (vec3_mm_t){.vec3 = {scalar, scalar, scalar}}));
+}
+
+inline vec3
+vec3cross(const vec3 v1, const vec3 v2) {
+	return((vec3){v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x});
+}
+
+inline float
+vec3dot(const vec3 v1, const vec3 v2) {
+	const vec3 span = vec3_mm_mul((vec3_mm_t){.vec3 = v1}, (vec3_mm_t){.vec3 = v2});
+    return(span.x + span.y + span.z);
+}
+
+inline float
+vec3len(const vec3 v) {
+	return (sqrtf(vec3dot(v, v)));
+}
+
+inline float
+vec3angle(const vec3 v1, const vec3 v2) {
+    return (atan2f(vec3len(vec3cross(v1, v2)), vec3dot(v1, v2)));
+}
+
+inline vec3
+vec3neg(const vec3 v) {
+    return((vec3){-v.x, -v.y, -v.z});
+}
+
+inline vec3
+vec3div(const vec3 v1, const vec3 v2) {
+    return(vec3_mm_div((vec3_mm_t){.vec3 = v1}, (vec3_mm_t){.vec3 = v2}));
+}
+
+inline vec3
+vec3divValue(const vec3 v, float f) {
+    if (f != 0.0f) {
+        return (vec3div(v, {f, f, f}));
+    } else {
+		return (v);
+    }
+}
+
+inline float
+distance3(const vec3 v1, const vec3 v2) {
+	const vec3 tmp = vec3sub(v1, v2);
+    return(vec3dot(tmp, tmp));
+}
+
+inline vec3
+vec3perpendicular(const vec3 v) {
+    float min = fabs(v.x);
+    vec3 cardinalAxis = (vec3){1.0f, 0.0f, 0.0f};
+    if (fabsf(v.y) < min) {
+        min = fabs(v.y);
+        cardinalAxis = (vec3){0.0f, 1.0f, 0.0f};
+    }
+    if (fabsf(v.z) < min) {
+        cardinalAxis = (vec3){0.0f, 0.0f, 1.0f};
+    }
+    return ((vec3){v.y*cardinalAxis.z - v.z*cardinalAxis.y, v.z*cardinalAxis.x - v.x*cardinalAxis.z, v.x*cardinalAxis.y - v.y*cardinalAxis.x});
+}
+
+inline vec3
+vec3norm(const vec3 v) {
+    float length = vec3len(v);
+    if (length != 0.0f) {
+        float inv_length = 1.0f/length;
+    	return((vec3){v.x * inv_length, v.y * inv_length, v.z * inv_length});
+    }
+	return (v);
 }
 
 inline vec3
@@ -38,105 +138,6 @@ rotatePitch(const vec3 point, const vec3 center, float angle_rad) {
     float rotatedY = center.y + (point.y - center.y) * cosTheta - (point.z - center.z) * sinTheta;
     float rotatedZ = center.z + (point.y - center.y) * sinTheta + (point.z - center.z) * cosTheta;
     return {point.x, rotatedY, rotatedZ};
-}
-
-inline float
-distance2(vec2 v1, vec2 v2) {
-    return (pow(v2.x - v1.x, 2) + pow(v2.y - v1.y, 2));
-}
-
-inline float
-distance3(vec3 v1, vec3 v2) {
-    return(pow(v2.x - v1.x, 2) + pow(v2.y - v1.y, 2) + pow(v2.z - v1.z, 2));
-}
-
-inline vec3
-vec3add(const vec3 v1, const vec3 v2) {
-	return((vec3){v1.x + v2.x, v1.y + v2.y, v1.z + v2.z});
-}
-
-inline vec3
-vec3sub(const vec3 v1, const vec3 v2) {
-	return((vec3){v1.x - v2.x, v1.y - v2.y, v1.z - v2.z});
-}
-
-inline vec3
-vec3addValue(const vec3 v, const float add) {
-	return((vec3){v.x + add, v.y + add, v.z + add});
-}
-
-inline vec3
-vec3subValue(const vec3 v, const float add) {
-    return((vec3){v.x - add, v.y - add, v.z - add});
-}
-
-inline vec3
-vec3scale(const vec3 v, const float scalar) {
-	return((vec3){v.x*scalar, v.y*scalar, v.z*scalar});
-}
-
-inline vec3
-vec3mul(const vec3 v1, const vec3 v2) {
-	return((vec3){v1.x*v2.x, v1.y*v2.y, v1.z*v2.z});
-}
-
-inline vec3
-vec3cross(const vec3 v1, const vec3 v2) {
-	return((vec3){v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x});
-}
-
-inline vec3
-vec3perpendicular(const vec3 v) {
-    float min = fabs(v.x);
-    vec3 cardinalAxis = (vec3){1.0f, 0.0f, 0.0f};
-    if (fabsf(v.y) < min) {
-        min = fabs(v.y);
-        cardinalAxis = (vec3){0.0f, 1.0f, 0.0f};
-    }
-    if (fabsf(v.z) < min) {
-        cardinalAxis = (vec3){0.0f, 0.0f, 1.0f};
-    }
-    return ((vec3){v.y*cardinalAxis.z - v.z*cardinalAxis.y, v.z*cardinalAxis.x - v.x*cardinalAxis.z, v.x*cardinalAxis.y - v.y*cardinalAxis.x});
-}
-
-inline float
-vec3len(const vec3 v) {
-	return (sqrtf(v.x*v.x + v.y*v.y + v.z*v.z));
-}
-
-inline float
-vec3lenSqr(const vec3 v) {
-    return (v.x*v.x + v.y*v.y + v.z*v.z);
-}
-
-inline float
-vec3dot(const vec3 v1, const vec3 v2) {
-    return(v1.x*v2.x + v1.y*v2.y + v1.z*v2.z);
-}
-
-inline float
-vec3angle(const vec3 v1, const vec3 v2) {
-    return (atan2f(vec3len(vec3cross(v1, v2)), vec3dot(v1, v2)));
-}
-
-inline vec3
-vec3neg(const vec3 v) {
-    return((vec3){-v.x, -v.y, -v.z});
-}
-
-inline vec3
-vec3div(const vec3 v1, const vec3 v2) {
-    return((vec3){v1.x/v2.x, v1.y/v2.y, v1.z/v2.z});
-}
-
-inline vec3
-vec3norm(const vec3 v) {
-    float length = vec3len(v);
-    if (length != 0.0f) {
-        float inv_length = 1.0f/length;
-    	return((vec3){v.x * inv_length, v.y * inv_length, v.z * inv_length});
-    }
-	return (v);
 }
 
 /*
